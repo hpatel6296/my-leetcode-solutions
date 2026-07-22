bfbf54b7c31d90567e36009de3e4c558863dcb6a
Harsh Patel <harshp6296@gmail.com>
2026-07-22 11:39:14 +0530

    Time: 0 ms (100.00%) | Memory: 26.6 MB (74.18%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,3 +1,29 @@
+// -> Using HashMap by counting frequancy
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+         int n = grid.size();
+         int total = n * n;
+         vector<int> freq(total + 1,0);
+
+         for(const auto& row : grid){
+            for(int num : row){
+                freq[num]++;
+            }
+         }
+         int repeNum = -1, misNum = -1;
+         for(int i=0;i<= n*n;i++){
+            if(freq[i] == 2)
+               repeNum = i;
+            if(freq[i] == 0)
+               misNum = i;
+         }
+         return {repeNum,misNum};
+    }
+};
+
+/*
+ -> Using Formula sum = n(n+1)/2
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
@@ -23,3 +49,4 @@ public:
         return ans;
     }
 };
+*/
