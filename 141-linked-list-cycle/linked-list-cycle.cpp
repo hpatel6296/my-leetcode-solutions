ed0abb4afb7a27ff1c0d7b8dffd6484c3840ce8c
Harsh Patel <harshp6296@gmail.com>
2026-07-24 17:11:20 +0530

    Time: 15 ms (9.65%) | Memory: 14.3 MB (11.77%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -6,6 +6,34 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+ // --> Using HashMap methode 
+
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        //Hash set
+        if(head == NULL || head->next == NULL){
+            return false;
+        }
+        unordered_set<ListNode*> node;
+        ListNode* temp = head;
+        while(temp->next != NULL){
+            // if temp is alredy apiarece ones in while 
+            if(node.count(temp)>0){
+                return true;
+            }
+            // store temp in node 
+            node.insert(temp);
+            temp = temp->next;
+        }
+        return false;
+    }
+};
+/*
+--> Using Two poinser Slow and fast where faster travese  fast than slower pointer 
+
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
@@ -24,3 +52,4 @@ public:
         return false;
     }
 };
+*/
