05e8e3cf1ccd0df0f5a03d0ba0b88bcba930d5ef
Harsh Patel <harshp6296@gmail.com>
2026-07-27 19:23:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (7.52%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int>nums(m+n,0);
+        int i=0,j=0,k=0;
+        while(i<m && j<n){
+            if(nums1[i]>nums2[j]){
+                nums[k] = nums2[j];
+                j++;
+            }
+            else{
+                nums[k] = nums1[i];
+                i++;
+            }
+            k++;
+        }
+       while(i<m){
+           nums[k] = nums1[i];
+           k++;
+           i++;
+       }
+        while(j<n){
+            nums[k] = nums2[j];
+            j++;
+            k++;
+        }
+        for(int i=0;i<m+n;i++){
+            nums1[i] = nums[i];
+        }
+    }
+};
+
+/*
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int a = m-1;
         int b = n-1;
         int c = m+n-1;
@@ -17,3 +50,4 @@ public:
         }
     }
 };
+*/
