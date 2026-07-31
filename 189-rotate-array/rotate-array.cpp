3f41ef606d8dc3bb931e25802447699ef4889cab
Harsh Patel <harshp6296@gmail.com>
2026-07-31 15:16:21 +0530

    Time: 3 ms (7.98%) | Memory: 30.6 MB (6.32%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+       int n = nums.size();
+       if(k == 0){
+        return;
+       }
+       if(k%n == 0){
+        return ;
+       }
+       if(k>n){
+       k %= n;
+       }
+       vector<int> ans(n,0);
+       int x = k,i=0;
+       while(x>0){
+        ans[i] = nums[n-x];
+        x--;
+        i++;
+       }
+       int j=0;
+       while(i<n){
+         ans[i] = nums[j];
+         j++;
+         i++;
+       } 
+       for(int i=0;i<n;i++){
+          nums[i] = ans[i];
+       }
+    }
+};
+/*
+//-> Using Two Pointer Method
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k %= n;
 
@@ -11,3 +44,4 @@ public:
         reverse(nums.begin()+k,nums.end());
     }
 };
+*/
