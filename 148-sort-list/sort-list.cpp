3a86af1042403f70b22a33b4dc1028873ab0fe37
Harsh Patel <harshp6296@gmail.com>
2026-07-31 22:49:52 +0530

    Implement linked list sorting using array and bubble sort
    Added methods to convert a linked list to an array, sort it using bubble sort, and convert it back to a linked list. This approach is not efficient for large linked lists.

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -55,3 +55,61 @@ public:
         return marge(left,right);
     }
 };
+
+/*
+->converting link list into array 
+this method is not work on large link list     
+class Solution {
+public:
+    int listLength(ListNode* head){
+        if(head == NULL){
+            return 0;
+        }
+        int l=0;
+        while(head != NULL){
+            head = head->next;
+            l++;
+        }
+        return l;
+    }
+    void listToArray(ListNode* head,vector<int>& nums){
+        int i=0;
+        while(i<nums.size() && head != NULL){
+            nums[i] = head->val;
+            head = head->next;
+            i++;
+        }
+    }
+
+    void bubbleSort(vector<int>& nums){
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<nums.size()-i-1;j++){
+                if(nums[j]>nums[j+1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    void arrayToList(vector<int>& nums,ListNode* head){
+        int i=0;
+        while(i<nums.size() && head != NULL){
+            head->val = nums[i];
+            i++;
+            head = head->next;
+        }
+    }
+    ListNode* sortList(ListNode* head) {
+        int l = listLength(head);
+        vector<int> nums(l,0);
+        ListNode* temp=head;
+        listToArray(temp,nums);
+        bubbleSort(nums);
+        ListNode* temp1=head;
+        arrayToList(nums,temp1);
+        return head;
+    }
+};
+*/
