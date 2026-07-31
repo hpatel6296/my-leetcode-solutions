f6473743ed41b94d5cfe2687bab072eef3599be4
Harsh Patel <harshp6296@gmail.com>
2026-07-31 22:35:04 +0530

    Time: 22 ms (51.08%) | Memory: 57 MB (82.94%) - LeetSync

---
+++ 148-sort-list/sort-list.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* midle(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast->next != NULL && fast->next->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    ListNode* marge(ListNode* l1,ListNode* l2){
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        while(l1 != NULL && l2 != NULL){
+            if(l1->val <= l2->val){
+               tail->next = l1;
+               l1 = l1->next;
+            }
+            else{
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+        if(l1 != NULL){
+            tail->next = l1;
+        }
+        if(l2 != NULL){
+            tail->next = l2;
+        }
+        return dummy.next;
+    }
+    ListNode* sortList(ListNode* head) {
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        ListNode* mid = midle(head);
+        ListNode* rightHead = mid->next;
+        mid->next = NULL;
+
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(rightHead);
+
+        return marge(left,right);
+    }
+};
