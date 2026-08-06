0cf6709310167d52f2ed89002801e9bb04e729e5
Harsh Patel <harshp6296@gmail.com>
2026-08-06 22:39:03 +0530

    Time: 5 ms (4.44%) | Memory: 71.9 MB (15.18%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,12 +1,8 @@
+//Kadane's Algorithm
+
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        if(nums.size() == 0){
-           return 0;
-       }
-       if(nums.size() == 1){
-           return nums[0];
-       }
        int maxSum = INT_MIN;
        int currSum = 0;
        for(int i=0;i<nums.size();i++){
