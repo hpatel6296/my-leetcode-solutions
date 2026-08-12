fcc62f5c33ca14ec022ff6fbaa8de4d8aad8085f
Harsh Patel <harshp6296@gmail.com>
2026-08-12 21:23:39 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (59.75%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    set<vector<int>> s;
-    void combiSum(vector<int>& arr,int tar,int idx,vector<vector<int>>& ans,vector<int>& combin){
-        if(tar < 0 || idx == arr.size()){
-            return ;
-        }
-        if(tar == 0){
-            if(s.find(combin) == s.end()){
-            ans.push_back({combin});
-            s.insert(combin);
-            }
-        }
-        combin.push_back(arr[idx]);
-        //single element
-        combiSum(arr,tar-arr[idx],idx+1,ans,combin);
-        //same element take multipale time 
-        combiSum(arr,tar-arr[idx],idx,ans,combin);
-        combin.pop_back();
-        //No element take 
-        combiSum(arr,tar,idx+1,ans,combin);
+    void findSubArray(vector<int>& candidantes,int target,int idx,vector<vector<int>> &ans,vector<int> &an){
+           if(target < 0 || idx >= candidantes.size()){
+              return ;
+           }
+           if(target == 0){
+             ans.push_back(an);
+             return;
+           }
+           if(idx<candidantes.size()){
+           an.push_back(candidantes[idx]);
+           findSubArray(candidantes,target-candidantes[idx],idx,ans,an);
+           //findSubArray(candidantes,target-candidantes[idx],idx+1,ans,an);
+           an.pop_back(); 
+           findSubArray(candidantes,target,idx+1,ans,an);
+           }
+
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int> combi;
-        combiSum(candidates,target,0,ans,combi);
-        return ans;
+          vector<vector<int>> ans;
+          vector<int> an;
+          findSubArray(candidates,target,0,ans,an);
+          return ans;
     }
 };
